e1182824c866e00396fef8f3dfd0a030f03a0c85
tony-tomass <tonynguyen984@gmail.com>
2023-09-15 12:57:06 -0500

    Added TextWatcher code that does not work

--- app/src/main/java/com/example/repairorder/MainActivity.java
+++ app/src/main/java/com/example/repairorder/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.repairorder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
@@ -31,6 +33,43 @@ public class MainActivity extends AppCompatActivity {
             total_value.setText("$" + total);
         }
     };
+/*
+    TextWatcher textWatcher = new TextWatcher() {
+        double cost_paint, cost_tech, cost_insp, cost_parts, cost_labor;
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            cost_paint = 0;
+            cost_tech = 0;
+            cost_insp = 0;
+            cost_parts = 0;
+            cost_labor = 0;
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            cost_paint = Double.parseDouble(paint_edit.getText().toString());
+            cost_tech = Double.parseDouble(technician_edit.getText().toString());
+            cost_insp = Double.parseDouble(inspection_edit.getText().toString());
+            cost_parts = Double.parseDouble(parts_edit.getText().toString());
+            cost_labor = Double.parseDouble(labor_edit.getText().toString());
+
+            double sub = cost_paint + cost_tech + cost_insp + cost_parts + cost_labor;
+            double tax = sub / 20;
+            double total = sub + tax;
+
+
+            subtotal_value.setText("$" + sub);
+            tax_value.setText("$" + tax);
+            total_value.setText("$" + total);
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
+
+ */
     Button submitbutton;
     EditText technician_edit, order_edit, inspection_edit, paint_edit, parts_edit, labor_edit;
     TextView subtotal_value, tax_value, total_value;
@@ -55,5 +94,15 @@ public class MainActivity extends AppCompatActivity {
 
         submitbutton.setOnClickListener(submitListener);
         //submitbutton.setText("hello");
+
+        /*
+        order_edit.addTextChangedListener(textWatcher);
+        technician_edit.addTextChangedListener(textWatcher);
+        inspection_edit.addTextChangedListener(textWatcher);
+        paint_edit.addTextChangedListener(textWatcher);
+        parts_edit.addTextChangedListener(textWatcher);
+        labor_edit.addTextChangedListener(textWatcher);
+
+         */
     }
 }
